eacdbe1726e90b16554620c9a6aaa67c641cb6a4
はるか <73038051+Harukaa428@users.noreply.github.com>
2024-07-23 19:52:31 +0700

    Add files via upload

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy zip here
+#gh auth login
+filename=$(ls *.zip)
+
+# Create a tag and release using the filename (without .zip extension)
+version=${filename%.zip}
+
+git tag -a "$version" -m "Release $version"
+
+git push origin "$version"
+
+gh release create "$version" "$filename" -t "Release $version" -n "Release notes"
